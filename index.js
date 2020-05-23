012b867c0b9474a308116c1cdacab162e4610fbf
paulizio <pauli.nordman@hotmail.com>
2020-05-23 12:17:21 +0300

    fixed port

--- index.js
+++ index.js
@@ -92,8 +92,8 @@ app.delete('/api/persons/:id',(req,res)=>{
 
 
 
-  const PORT=process.env.PORT||3001
-  app.listen(PORT,()=>{
-    console.log(`Server running on port ${PORT}`)
-  })
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
 
