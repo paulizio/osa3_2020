77c3aff9da157bbfb182d8febb1adb7be503362b
paulizio <pauli.nordman@hotmail.com>
2020-05-23 19:44:43 +0300

    fixed info

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-require('dotenv').config()
 const express = require('express')
-const cors=require('cors')
 const app = express()
-const morgan = require('morgan')
-const Person=require('./models/person')
-morgan('tiny')
-app.use(express.json())
-app.use(cors())
 app.use(express.static('build'))
+app.use(express.json())
+require('dotenv').config()
+const morgan = require('morgan')
+morgan('tiny')
+const cors=require('cors')
+const Person=require('./models/person')
+app.use(cors())
 let persons=[
     { 
       "name": "Arto Hellas", 
@@ -36,25 +36,34 @@ let persons=[
 app.use(morgan(morganLogger))
 
 
-app.get('/api/persons',(req,res)=>{
+app.get('/api/persons',(req,res,next)=>{
   Person.find({}).then(persons=>{
-    res.json(persons.map(person=>person.toJSON()))
+    res.json(persons)
   })
+  .catch(error=>next(error))
 })
-app.get('/info',(req,res)=>{
-    let amount=persons.length
-    console.log('amount of people: ', amount)
+app.get('/info',(req,res,next)=>{
+    // let amount=persons.length
+    // console.log('amount of people: ', amount)
     let date=new Date()
-    console.log('Date: ',date)
-    res.send(`<p>Phonebook has info for ${amount} people</p>
-    <p>${date}</p>`)
+    // console.log('Date: ',date)
+    // res.send(`<p>Phonebook has info for ${amount} people</p>
+    // <p>${date}</p>`)
+    Person.find({}).then(person=>{
+      
+      res.send(`<p>Phonebook has info for ${person.length} people</p>
+      <p>${date}</p>`)
+    })
+  }
+)
 
-})
+
+
 // const generateId = () => {
 
 //     return Math.floor(Math.random()*10000000000000000000)
 //   }
-app.post('/api/persons',(request,response)=>{   
+app.post('/api/persons',(request,response,next)=>{   
     const body=request.body
     if(!body.name||!body.number){
 return response.status(400).json({
@@ -76,28 +85,45 @@ return response.status(400).json({
    person.save().then(savedPerson=>{
      response.json(savedPerson)
    })
+   .catch(error=>next(error))
 }})
-app.get('/api/persons/:id',(req,res)=>{
-    let id=Number(req.params.id)
-    console.log('id: ',id)
-    console.log('typeof id:', typeof id)
-    let person=persons.find(person=>person.id===id)
-    console.log('Person.id type: ',typeof person)
-    if(person){
-    res.json(person)
+app.get('/api/persons/:id',(req,res,next)=>{
+    // let id=Number(req.params.id)
+    // console.log('id: ',id)
+    // console.log('typeof id:', typeof id)
+    // let person=persons.find(person=>person.id===id)
+    // console.log('Person.id type: ',typeof person)
+    // if(person){
+    // res.json(person)
+    Person.findById(req.params.id)
+    .then(person=>{
+      if(person){
+        res.json(person.toJSON())
+      
     }else{
         res.status(404).end()
     }
-})
-app.delete('/api/persons/:id',(req,res)=>{
-    let id=Number(req.params.id)
-    persons=persons.filter(p=>p.id!==id)
-    res.status(204).end()
+  })
+  .catch(error=>next(error))
 })
 
+app.delete('/api/persons/:id',(req,res,next)=>{
+Person.findByIdAndRemove(req.params.id)
+.then(result=>{
+  res.status(204).end()
+})
+.catch(error=>next(error))
+})
 
 
-
+const errorHandler=(error,req,res,next)=>{
+  console.log(error.message)
+if(error.name==='CastError'){
+  return res.status(400).send({error:'malformatted id'})
+}
+next(error)
+}
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
