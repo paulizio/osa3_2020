3b6ebe6ca5e80d1bed6c2fb22c056a55e874cdbb
paulizio <pauli.nordman@hotmail.com>
2020-05-23 13:22:18 +0300

    added build

---
+++ build/precache-manifest.7c884717c11db7b69a70400710501629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fa369ee16230d48c4aae8bf1098aee7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afcddd6349917d84a4ff",
+    "url": "/static/css/main.bdef013b.chunk.css"
+  },
+  {
+    "revision": "27bd1cdff6215b46af31",
+    "url": "/static/js/2.fedff18b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fedff18b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afcddd6349917d84a4ff",
+    "url": "/static/js/main.d068be4b.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
