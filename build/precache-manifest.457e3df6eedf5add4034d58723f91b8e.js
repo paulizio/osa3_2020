37961f55e23c1583601949ee7a4e99617fea5e6d
paulizio <pauli.nordman@hotmail.com>
2020-05-24 11:59:46 +0300

    heroku version

---
+++ build/precache-manifest.457e3df6eedf5add4034d58723f91b8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20a473a9f8210480100a33ccc067645",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fd169ba21b9651bae7e",
+    "url": "/static/css/main.bdef013b.chunk.css"
+  },
+  {
+    "revision": "27bd1cdff6215b46af31",
+    "url": "/static/js/2.fedff18b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fedff18b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd169ba21b9651bae7e",
+    "url": "/static/js/main.61b8386b.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
